807c6755ba53e239883b408d6cd4dd0ab0f544e2
nelsonic <nelson+github@dwyl.com>
2022-12-28 13:17:04 +0000

    remove swoosh. not sending email in this demo #4

--- config/config.exs
+++ config/config.exs
@@ -23,15 +23,6 @@ config :app, AppWeb.Endpoint,
   pubsub_server: App.PubSub,
   live_view: [signing_salt: "4GJwZxxe"]
 
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :app, App.Mailer, adapter: Swoosh.Adapters.Local
-
 # Configure esbuild (the version is required)
 config :esbuild,
   version: "0.14.41",
